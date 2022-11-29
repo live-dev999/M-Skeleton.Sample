3e3288d372a80d5a024a25389e813e10a26bbc5d
Denis Prokharchyk <live-dev@hotmail.com>
2022-11-29 07:36:10 +0300

    feat(#2): add dbo table script

---
+++ src/dbo/tables/DDL - MSkeleton.Sample - config.Config.sql
@@ -0,0 +1,16 @@
+﻿--use Skeleton.Sample
+--go 
+
+if object_id('config.Config', 'U') is not null
+	drop table config.Config
+
+create table config.Config
+(
+	config_id int not null identity(1,1),
+	config_key nvarchar(20) not null,
+	config_value nvarchar (max) not null,
+	modified_date datetime not null,
+		
+	constraint PK_Config primary key (config_id)	
+)
+go
